ae73c331a614c89b40ffa843d9f8cb69c3831f53
Andrés Fernández <a01281054@tec.mx>
2026-02-22 18:34:21 -0600

    feat: added all customer logic and respective main line codes

--- file.py
+++ file.py
@@ -24,9 +24,9 @@ class Hotel(ABC):
     @abstractmethod
     def display_hotel_info(self, hotel_id): pass
     @abstractmethod
-    def modify_hotel_info(self, hotel_id, new_name, new_rooms): pass
+    def modify_hotel_info(self, hotel_id, new_name = None, new_rooms = None): pass
     @abstractmethod
-    def reserve_room(self): pass
+    def reserve_room(self, hotel_id): pass
     @abstractmethod
     def cancel_reservation(self): pass
 
@@ -35,13 +35,13 @@ class Customer(ABC):
     '''This abstract class defines the customer functionality.'''
 
     @abstractmethod
-    def create_customer(self): pass
+    def create_customer(self, new_data_file): pass
     @abstractmethod
-    def delete_customer(self): pass
+    def delete_customer(self, selected_id): pass
     @abstractmethod
-    def display_cust_info(self): pass
+    def display_cust_info(self, customer_id): pass
     @abstractmethod
-    def modify_cust_info(self): pass
+    def modify_cust_info(self, customer_id, new_name = None, new_email = None): pass
 
 
 class Reservation(ABC):
@@ -94,15 +94,15 @@ class HotelSystem(Hotel, Reservation):
             with open(self.master_file, 'w') as m_file:
                 json.dump(master_list, m_file, indent=4)
 
-            print(f"Import finished: {added_count} new hotels added to {self.master_file}.")
+            print(f"Import finished: {added_count} new hotels added to {self.master_file}.\n")
 
         # Error handling if source file is not found.
         except FileNotFoundError:
-            print(f"Error: Source file '{new_data_file}' not found.")
+            print(f"Error: Source file '{new_data_file}' not found.\n")
 
         # Error handling if json file data is invalid.
         except json.JSONDecodeError:
-            print(f"Error: '{new_data_file}' is not a valid JSON file.")
+            print(f"Error: '{new_data_file}' is not a valid JSON file.\n")
 
     def delete_hotel(self, selected_id):
         '''Function to delete hotels by hotel ID from master file.'''
@@ -118,14 +118,14 @@ class HotelSystem(Hotel, Reservation):
 
         # Error handling by comparing hotel entries to check if a hotel has been deleted.
         if len(updated_list) == initial_count:
-            print(f"Error: Hotel ID '{selected_id}' not found in {self.master_file}.")
+            print(f"Error: Hotel ID '{selected_id}' not found in {self.master_file}.\n")
             return
 
         # Store updated hotel data in json master file.
         with open(self.master_file, 'w') as m_file:
             json.dump(updated_list, m_file, indent=4)
 
-        print(f"Success: Hotel with ID '{selected_id}' has been deleted.")
+        print(f"Success: Hotel with ID '{selected_id}' has been deleted.\n")
 
     def display_hotel_info(self, hotel_id = "all"):
         '''Function to display hotels info. from json master file.'''
@@ -136,7 +136,7 @@ class HotelSystem(Hotel, Reservation):
 
         # Check to see if there are not any registered hotels.
         if not master_list:
-            print(f"\n The master file '{self.master_file}' does not have any registered hotels.")
+            print(f"\n The master file '{self.master_file}' does not have any registered hotels.\n")
             return
 
         # Determine which hotels to show based on hotel ID or if all are requested.
@@ -148,7 +148,7 @@ class HotelSystem(Hotel, Reservation):
 
         # Check if the requested hotel ID exists.
         if not display_list:
-            print(f"Error: No hotel found with ID '{hotel_id}'.")
+            print(f"Error: No hotel found with ID '{hotel_id}'.\n")
             return
 
         # Get the hotel information from the list to be displayed and print.
@@ -157,7 +157,9 @@ class HotelSystem(Hotel, Reservation):
             name = hotel.get('name', 'N/A')
             loc = hotel.get('location', 'N/A')
             rooms = hotel.get('rooms', 0)
-            print(f"Hotel ID: {h_id:<5} | Name: {name:<20} | Location: {loc:<20} | Total rooms: {rooms}")
+            a_rooms = hotel.get('available_rooms', 0)
+            print(f"Hotel ID: {h_id:<3} | Name: {name:<15} | Location: {loc:<15} | "
+                  f"Available rooms: {a_rooms:<5} | Total rooms: {rooms}\n")
 
         print(f"\nHotels displayed: {len(display_list)}.\n")
 
@@ -185,21 +187,51 @@ class HotelSystem(Hotel, Reservation):
 
             # Check to see if the hotel selected by its ID is valid.
             if not found:
-                print(f"Error: Hotel ID '{hotel_id}' not found.")
+                print(f"Error: Hotel ID '{hotel_id}' not found.\n")
                 return
 
             # Store hotel info. changes.
             with open(self.master_file, 'w') as m_file:
                 json.dump(master_list, m_file, indent=4)
 
-            print(f"Success: Hotel ID '{hotel_id}' information updated.")
+            print(f"Success: Hotel ID '{hotel_id}' information updated.\n")
 
         # Error handling for invalid new rooms number.
         except ValueError:
-            print("Error: The number of rooms must be a valid number.")
+            print("Error: The number of rooms must be a valid number.\n")
 
+    def reserve_room(self, hotel_id):
+        '''Function to reserve a room in a hotel by decrementing the count in json master file.'''
 
-    def reserve_room(self): pass
+        # Read existing hotels information from master file.
+        with open(self.master_file, 'r') as m_file:
+            master_list = json.load(m_file)
+
+        found = False
+        # Check hotel by ID to make modification in available rooms count
+        for hotel in master_list:
+            if str(hotel.get('hotel_id')) == str(hotel_id):
+                current_rooms = hotel.get('available_rooms', 0)
+
+                # Check if there are available rooms, then make reservation
+                if current_rooms > 0:
+                    hotel['available_rooms'] = current_rooms - 1
+                    found = True
+                    print(f"Success: Room reserved at {hotel['name']}. Remaining rooms: {hotel['available_rooms']}.\n")
+
+                # Error handling, display message when there are not any available rooms
+                else:
+                    print(f"Error: No rooms available at {hotel['name']}.\n")
+                    return
+                break
+
+        if not found:
+            print(f"Error: Hotel ID '{hotel_id}' not found.\n")
+            return
+
+        # Write back information to master file
+        with open(self.master_file, 'w') as m_file:
+            json.dump(master_list, m_file, indent=4)
 
     # Reservation system.
     def create_reservation(self): pass
@@ -207,48 +239,241 @@ class HotelSystem(Hotel, Reservation):
 
 
 class CustomerSystem(Customer):
+    '''
+    This class implements a system to manage customers information
+    into a JSON file named master_customers. It has a list of
+    dictionaries; the following information can be found in this
+    file: customer_id, name, age, email.
+    '''
 
+    '''
     def create_customer(self): pass
     def delete_customer(self): pass
     def display_cust_info(self): pass
     def modify_cust_info(self): pass
+    '''
+
+    # Master file initialization containing all hotels data.
+    def __init__(self, master_file="master_customers.json"):
+        self.master_file = master_file
+
+        # Check if master file exists, if not create a new one.
+        if not os.path.exists(self.master_file):
+            with open(self.master_file, 'w') as f:
+                json.dump([], f)
+
+    def create_customer(self, new_data_file):
+        '''Function to create customer data into master file.'''
+
+        try:
+            # Read new customer data from provided json file.
+            with open(new_data_file, 'r') as s_file:
+                new_customers = json.load(s_file)
+
+            # Read existing customer data from master file.
+            with open(self.master_file, 'r') as m_file:
+                master_list = json.load(m_file)
+
+            # Get all existing customer IDs.
+            existing_ids = {c.get('customer_id') for c in master_list}
+            added_count = 0  # New customer count initialization.
+
+            # Check to see if hotel data from provided file is new by hotel ID.
+            for customer in new_customers:
+                if customer.get('customer_id') not in existing_ids:
+                    master_list.append(customer)
+                    added_count += 1
+
+            # Write customer data into json master file.
+            with open(self.master_file, 'w') as m_file:
+                json.dump(master_list, m_file, indent=4)
+
+            print(f"Import finished: {added_count} new customers added to {self.master_file}.\n")
+
+        # Error handling if source file is not found.
+        except FileNotFoundError:
+            print(f"Error: Source file '{new_data_file}' not found.\n")
+
+        # Error handling if json file data is invalid.
+        except json.JSONDecodeError:
+            print(f"Error: '{new_data_file}' is not a valid JSON file.\n")
+
+
+    def delete_customer(self, selected_id):
+        '''Function to delete customers by customer ID from master file.'''
+
+        # Read existing customer data from json master file.
+        with open(self.master_file, 'r') as m_file:
+            master_list = json.load(m_file)
+
+        # Initialization of length to keep track of customer entries.
+        initial_count = len(master_list)
+        # Filter to keep all customers except the customer selected to be deleted.
+        updated_list = [c for c in master_list if str(c.get('customer_id')) != str(selected_id)]
+
+        # Error handling by comparing customer entries to check if a customer has been deleted.
+        if len(updated_list) == initial_count:
+            print(f"Error: Customer ID '{selected_id}' not found in {self.master_file}.\n")
+            return
+
+        # Store updated customer data in json master file.
+        with open(self.master_file, 'w') as m_file:
+            json.dump(updated_list, m_file, indent=4)
+
+        print(f"Success: Customer with ID '{selected_id}' has been deleted.\n")
+
+    def display_cust_info(self, customer_id="all"):
+        '''Function to display customers info. from json master file.'''
+
+        # Read existing customers information from master file.
+        with open(self.master_file, 'r') as m_file:
+            master_list = json.load(m_file)
+
+        # Check to see if there are not any registered customers.
+        if not master_list:
+            print(f"\n The master file '{self.master_file}' does not have any registered customers.\n")
+            return
+
+        # Determine which customers to show based on customer ID or if all are requested.
+        if str(customer_id).lower() == "all":
+            display_list = master_list
+        else:
+            # Filter by the specific customer ID and store info. in display list.
+            display_list = [c for c in master_list if str(c.get('customer_id')) == str(customer_id)]
+
+        # Check if the requested customer ID exists.
+        if not display_list:
+            print(f"Error: No customer found with ID '{customer_id}'.\n")
+            return
+
+        # Get the customer information from the list to be displayed and print.
+        for customer in display_list:
+            c_id = customer.get('customer_id', 'N/A')
+            name = customer.get('name', 'N/A')
+            age = customer.get('age', 0)
+            email = customer.get('email', 'N/A')
+            print(f"Customer ID: {c_id:<3} | Name: {name:<15} | Age: {age:<15} | "
+                  f"Email: {email:<5}\n")
+
+        print(f"\nCustomers displayed: {len(display_list)}.\n")
+
+    def modify_cust_info(self, customer_id, new_name = None, new_email = None):
+        '''Function to modify customers name and email in json master file.'''
+
+        try:
+            # Read existing customers information from master file.
+            with open(self.master_file, 'r') as m_file:
+                master_list = json.load(m_file)
+
+            found = False
+            for customer in master_list:
+                # Check/select the customer to be modified by its customer ID from master file.
+                if str(customer.get('customer_id')) == str(customer_id):
+                    # Update customer name.
+                    if new_name:
+                        customer['name'] = new_name
+
+                    # Update customer email.
+                    if new_email:
+                        customer['email'] = new_email
+                    found = True
+                    break
+
+            # Check to see if the customer selected by its ID is valid.
+            if not found:
+                print(f"Error: Customer ID '{customer_id}' not found.\n")
+                return
+
+            # Store customer info. changes.
+            with open(self.master_file, 'w') as m_file:
+                json.dump(master_list, m_file, indent=4)
+
+            print(f"Success: Customer ID '{customer_id}' information updated.\n")
+
+        # Error handling for invalid email.
+        except ValueError:
+            print("Error: Email must be a valid input.\n")
+
+
 
 
 def main():
-    system = HotelSystem()
+    system_hotel = HotelSystem()
+    system_customer = CustomerSystem()
 
     command = sys.argv[1]
-    arg = sys.argv[2]
+    hot_cust = sys.argv[2]
+    arg = sys.argv[3]
 
     # Checks command input to select hotel system feature.
     match command:
         case "create":
-            #arg = sys.argv[2]
-            system.create_hotel(arg)
+            if hot_cust == "hotel":
+                system_hotel.create_hotel(arg)
+            elif hot_cust == "customer":
+                system_customer.create_customer(arg)
+
         case "delete":
-            #arg = sys.argv[2]
-            system.delete_hotel(arg)
+            if hot_cust == "hotel":
+                system_hotel.delete_hotel(arg)
+            elif hot_cust == "customer":
+                system_customer.delete_customer(arg)
+
         case "display":
-            system.display_hotel_info(arg)
+            if hot_cust == "hotel":
+                system_hotel.display_hotel_info(arg)
+            elif hot_cust == "customer":
+                system_customer.display_cust_info(arg)
+
         case "modify":
+            '''
             # Checks command line whether only hotel name or rooms are to be modified.
-            if len(sys.argv) == 4:
-                h_id = sys.argv[2]
-                val = sys.argv[3]
+            if len(sys.argv) == 5:
+                h_id = sys.argv[3]
+                val = sys.argv[4]
 
                 # Check if the value is a number (rooms) or string (name).
                 if val.isdigit():
-                    system.modify_hotel_info(h_id, new_rooms = val)
+                    system_hotel.modify_hotel_info(h_id, new_rooms = val)
                 else:
-                    system.modify_hotel_info(h_id, new_name = val)
+                    system_hotel.modify_hotel_info(h_id, new_name = val)
 
             # Checks command line if hotel name and rooms will be modified.
-            elif len(sys.argv) == 5:
-                system.modify_hotel_info(sys.argv[2], sys.argv[3], sys.argv[4])
+            elif len(sys.argv) == 6:
+                system_hotel.modify_hotel_info(sys.argv[3], sys.argv[4], sys.argv[5])
 
             # Error handling if no hotel ID was given or no value to be changed.
             else:
-                print("Error: modify requires hotel ID and at least one value to change.")
+                print("Error: modify requires hotel ID and at least one value to change.\n")
+        '''
+            if hot_cust == "hotel":
+                # ... your existing hotel modify logic ...
+                if len(sys.argv) == 5:
+                    h_id, val = sys.argv[3], sys.argv[4]
+                    if val.isdigit():
+                        system_hotel.modify_hotel_info(h_id, new_rooms=val)
+                    else:
+                        system_hotel.modify_hotel_info(h_id, new_name=val)
+                elif len(sys.argv) == 6:
+                    system_hotel.modify_hotel_info(sys.argv[3], sys.argv[4], sys.argv[5])
+
+            elif hot_cust == "customer":
+                # Logic for modifying customer (Name or Email)
+                if len(sys.argv) == 5:
+                    c_id, val = sys.argv[3], sys.argv[4]
+                    # Since both are strings, we can check for an '@' to guess if it's an email
+                    if "@" in val:
+                        system_customer.modify_cust_info(c_id, new_email = val)
+                    else:
+                        system_customer.modify_cust_info(c_id, new_name = val)
+                elif len(sys.argv) == 6:
+                    # Modify both: python file.py modify customer <id> <name> <email>
+                    system_customer.modify_cust_info(sys.argv[3], sys.argv[4], sys.argv[5])
+                else:
+                    print("Error: modify customer requires ID and at least one value.")
+
+        case "reserve":
+            system_hotel.reserve_room(arg)
 
 if __name__ == '__main__':
     main()
